e584b562604abb1c12b74d49c2048920f40838d9
kelompok-pbo <76928995+kelompok-pbo@users.noreply.github.com>
2021-01-05 11:53:55 +0700

    Create Property.php

---
+++ Property.php
@@ -0,0 +1,10 @@
+<?php
+//cara penulisan class dan property OOP PHP
+class mobil {          //class mobil
+    var $warna;        //property merah
+    var $merek;        //property honda br-v
+    var $ukuran;.      //property 180
+
+
+}
+?>
